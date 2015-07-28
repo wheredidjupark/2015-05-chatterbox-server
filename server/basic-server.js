b1aff01555de98408ad5fdafa003523cc62b4974
Ju Hyeon Park <wheredidjupark@gmail.com>
2015-07-27 20:06:48 -0700

    run the server successfully after importing request-handler.js to basic-server.js

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,7 +1,7 @@
 /* Import node's http module: */
 var http = require("http");
-
-
+var handleRequest = require("./request-handler");
+//var handleRequest = requestHandler.requestHandler;
 // Every server needs to listen on a port with a unique number. The
 // standard port for HTTP servers is port 80, but that port is
 // normally already claimed by another server and/or not accessible
