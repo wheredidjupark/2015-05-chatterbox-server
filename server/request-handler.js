cf83c023e02a720cc0f317098d2f0105c85d5ca5
Ju Hyeon Park <wheredidjupark@gmail.com>
2015-07-29 14:50:27 -0700

    refactor POST, GET, and OPTIONS responses into the sendResponse function

--- server/request-handler.js
+++ server/request-handler.js
@@ -25,71 +25,73 @@ this file and include it in basic-server.js so that it actually works.
 var messages = [];
 
 var defaultCorsHeaders = {
-  "access-control-allow-origin": "*",
-  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": "content-type, accept",
-  "access-control-max-age": 10 // Seconds.
+    "access-control-allow-origin": "*",
+    "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "access-control-allow-headers": "content-type, accept",
+    "access-control-max-age": 10 // Seconds.
 };
 
 
 
+var sendResponse = function(statusCode, data, response) {
+    response.writeHead(statusCode, defaultCorsHeaders);
+    response.end(data);
+};
+
+
 var requestHandler = function(request, response) {
 
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
+    // Request and Response come from node's http module.
+    //
+    // They include information about both the incoming request, such as
+    // headers and URL, and about the outgoing response, such as its status
+    // and content.
+    //
+    // Documentation for both request and response can be found in the HTTP section at
+    // http://nodejs.org/documentation/api/
 
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
-  console.log("Serving request type " + request.method + " for url " + request.url);
+    // Do some basic logging.
+    //
+    // Adding more logging to your server can be an easy way to get passive
+    // debugging help, but you should always be careful about leaving stray
+    // console.logs in your code.
+    console.log("Serving request type " + request.method + " for url " + request.url);
 
-  // The outgoing status.
-  var statusCode = 200; 
+    // The outgoing status.
+    var statusCode = 200;
 
-  // See the note below about CORS headers.
-  // Tell the client we are sending them JSON.
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  defaultCorsHeaders['Content-Type'] = "application/json";
+    // See the note below about CORS headers.
+    // Tell the client we are sending them JSON.
+    // You will need to change this if you are sending something
+    // other than plain text, like JSON or HTML.
+    defaultCorsHeaders['Content-Type'] = "application/json";
 
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
+    // .writeHead() writes to the request line and headers of the response,
+    // which includes the status and all headers.
 
-if(request.method === "GET"){
-  //TODO: sending response for GET request
-  response.writeHead(statusCode, defaultCorsHeaders);
-  response.end(JSON.stringify({results: messages}));
-} else if (request.method === "POST"){
-  //TODO: sending response for POST request
-  statusCode = 201;
-  response.writeHead(statusCode, defaultCorsHeaders);
-  response.end(JSON.stringify({results: messages}));
-} else if (request.method === "OPTIONS"){
-  //TODO: sending response for OPTIONS request (i.e. cross origin request)
-  statusCode = 200;
-  response.writeHead(statusCode, defaultCorsHeaders);
-  response.end();
-}
+    if (request.method === "GET") {
+
+        sendResponse(statusCode, JSON.stringify({results:messages}), response);
+    } else if (request.method === "POST") {
+
+        statusCode = 201;
+        sendResponse(statusCode,JSON.stringify({results:messages}) , response);
+    } else if (request.method === "OPTIONS") {
+        //TODO: sending response for OPTIONS request (i.e. cross origin request)
+        sendResponse(statusCode, null, response);
+    }
 
 
-response.end();
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
+    response.end();
+    // Make sure to always call response.end() - Node may not send
+    // anything back to the client until you do. The string you pass to
+    // response.end() will be the body of the response - i.e. what shows
+    // up in the browser.
+    //
+    // Calling .end "flushes" the response's internal buffer, forcing
+    // node to actually send all the data over to the client.
 
-  
+
 
 
 
