0cd54d7ae9fdea1a076d54d088b245190b22d234
Ju Hyeon Park <wheredidjupark@gmail.com>
2015-07-29 16:06:56 -0700

    move utility codes into utils.js

--- server/request-handler.js
+++ server/request-handler.js
@@ -22,31 +22,7 @@ this file and include it in basic-server.js so that it actually works.
 // Another way to get around this restriction is to serve you chat
 // client from this domain by setting up static file serving.
 
-var messages = [];
-var objectId = 1;
-
-var defaultCorsHeaders = {
-    "access-control-allow-origin": "*",
-    "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-    "access-control-allow-headers": "content-type, accept",
-    "access-control-max-age": 10, // Seconds.
-    "Content-Type": "application/json"
-
-    // See the note below about CORS headers.
-    // Tell the client we are sending them JSON.
-    // You will need to change this if you are sending something
-    // other than plain text, like JSON or HTML.
-
-};
-
-
-
-var sendResponse = function(statusCode, data, response) {
-    response.writeHead(statusCode, defaultCorsHeaders);
-    response.end(data);
-};
-
-
+var utils = require("./utils");
 
 var requestHandler = function(request, response) {
 
@@ -67,46 +43,13 @@ var requestHandler = function(request, response) {
     console.log("Serving request type " + request.method + " for url " + request.url);
 
     // The outgoing status.
-    var statusCode = 200;
+    //var statusCode = 200;
 
     // .writeHead() writes to the request line and headers of the response,
     // which includes the status and all headers.
 
-    var collectData = function(request, callback) {
-        var data = "";
 
-        request.on("data", function(chunk) {
-            data += chunk;
-        });
-
-        request.on("end", function() {
-            callback(JSON.parse(data)); //the data will be in json format
-        });
-    };
-
-
-    var actions = {
-        "GET": function(request, response) {
-            sendResponse(statusCode, JSON.stringify({
-                results: messages
-            }), response);
-        },
-
-        "POST": function(request, response) {
-
-            statusCode = 201;
-            collectData(request, function(message){
-              message.objectId = ++objectId;
-              messages.push(message);
-              sendResponse(statusCode, JSON.stringify({results: messages}), response);
-            });
-        },
-        "OPTIONS": function(request, response) {
-            sendResponse(statusCode, null, response);
-        }
-    };
-
-    var action = actions[request.method];
+    var action = utils.actions[request.method];
     if (action) {
         action(request, response);
     } else {
---
+++ server/utils.js
@@ -0,0 +1,67 @@
+var messages = [];
+var objectId = 1;
+
+var defaultCorsHeaders = {
+    "access-control-allow-origin": "*",
+    "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "access-control-allow-headers": "content-type, accept",
+    "access-control-max-age": 10, // Seconds.
+    "Content-Type": "application/json"
+
+    // See the note below about CORS headers.
+    // Tell the client we are sending them JSON.
+    // You will need to change this if you are sending something
+    // other than plain text, like JSON or HTML.
+
+};
+
+
+
+var sendResponse = function(statusCode, data, response) {
+    response.writeHead(statusCode, defaultCorsHeaders);
+    response.end(data);
+};
+
+
+var collectData = function(request, callback) {
+    var data = "";
+
+    request.on("data", function(chunk) {
+        data += chunk;
+    });
+
+    request.on("end", function() {
+        callback(JSON.parse(data)); //the data will be in json format
+    });
+};
+
+
+var actions = {
+    "GET": function(request, response) {
+        sendResponse(200, JSON.stringify({
+            results: messages
+        }), response);
+    },
+
+    "POST": function(request, response) {
+
+        //statusCode = 201;
+        collectData(request, function(message) {
+            message.objectId = ++objectId;
+            messages.push(message);
+            sendResponse(201, JSON.stringify({
+                results: messages
+            }), response);
+        });
+    },
+    "OPTIONS": function(request, response) {
+        sendResponse(200, null, response);
+    }
+};
+
+module.exports.actions = actions;
+module.exports.collectData = collectData;
+module.exports.sendResponse = sendResponse;
+module.exports.defaultCorsHeaders = defaultCorsHeaders;
+module.exports.objectId = objectId;
+module.exports.messages = messages;
